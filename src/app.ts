bc777afc640e409b1f596236f272c4837a7b911f
Mohammed Khallaf <mohammed.khallaf.1997@gmail.com>
2024-10-05 15:21:47 +0300

    add error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import { connectDB } from "./config/database";
 import { specs } from "./config/swagger";
+import { errorHandler } from "./middleware/error-handler";
 
 const app = express();
 
@@ -12,5 +13,7 @@ app.use(express.json());
 connectDB();
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+app.use(errorHandler);
 
 export default app;
